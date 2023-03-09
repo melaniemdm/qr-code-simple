00b59dccfdcf38ad1c21892b8642f7aaa5f55ee3
melaniemdm <melanieborja.pro@gmail.com>
2023-03-09 13:43:41 +0100

    init projet feature(js) index.js feature(css) style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
-import React from 'react'
-import styles from './styles.module.css'
+import './styles.module.css'
+import QRCode from 'react-qr-code'
+import React, { useState } from 'react'
+export function EnterUrl() {
+  const [url, setUrl] = useState('')
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  function recupUrl() {
+    const urlInput = document.querySelector('#input').value
+    setUrl(urlInput)
+  }
+  return (
+    <div className='container'>
+      <div className='title'>Mettre l'url du site internet</div>
+      <input placeholder=' http://' id='input' />
+      <button onClick={() => recupUrl()}>Genere QR code</button>
+
+      {QrCodeGenerate(url)}
+    </div>
+  )
+}
+
+function QrCodeGenerate(props) {
+  if (props.url && props.url !== '') {
+    return (
+      <div className='containerQrCode'>
+        <QRCode id='qrcode' value={decodeURIComponent(props.url)} />
+      </div>
+    )
+  }
 }
